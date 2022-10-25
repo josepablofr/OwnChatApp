f055cdce0cd078693d5b745016ad30379c7d204e
Jose Pablo Flores Rabasa <josepablofr@MacBook-Pro-de-Jose.local>
2022-10-25 13:15:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ownchatapp/activity/MessagesActivity.kt
@@ -0,0 +1,55 @@
+package com.example.ownchatapp.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.compose.ui.messages.MessagesScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.compose.ui.theme.StreamShapes
+
+class MessagesActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val channelId = intent.getStringExtra(KEY_CHANNEL_ID)
+
+        if (channelId == null) {
+            finish()
+            return
+        }
+
+        setContent {
+            ChatTheme (
+                shapes = StreamShapes.defaultShapes().copy(
+                    avatar = RoundedCornerShape(8.dp),
+                    attachment = RoundedCornerShape(16.dp),
+                    myMessageBubble = RoundedCornerShape(16.dp),
+                    otherMessageBubble = RoundedCornerShape(16.dp),
+                    inputField = RectangleShape
+                )
+                ){
+                MessagesScreen(
+                    channelId = channelId,
+                    messageLimit = 30,
+                    onBackPressed = {finish()}
+                )
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY_CHANNEL_ID = "channelID"
+
+        fun getIntent(context: Context, channelId: String): Intent {
+            return Intent(context, MessagesActivity::class.java).apply {
+                putExtra(KEY_CHANNEL_ID, channelId)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ownchatapp/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.ownchatapp.util
+
+object Constants {
+    const val MIN_USERNAME_LENGHT = 3
+}
